e62b75c4e354303c9d9b6c176f8ed78099e52aad
jeffmorice <jmorice.anim@gmail.com>
2019-01-30 19:29:12 -0600

    Edited MediaController to pass MediaObjects table data to the Index view. Created Index view for displaying that data.

--- BacklogManager/Controllers/MediaController.cs
+++ BacklogManager/Controllers/MediaController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BacklogManager.Data;
 using Microsoft.AspNetCore.Mvc;
+using BacklogManager.Models;
 
 namespace BacklogManager.Controllers
 {
     public class MediaController : Controller
     {
+        private readonly MediaObjectDbContext context;
+
+        public MediaController(MediaObjectDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return Content("Hello, World!");
+            List<MediaObject> mediaList = context.MediaObjects.ToList();
+
+            return View(mediaList);
         }
     }
 }
