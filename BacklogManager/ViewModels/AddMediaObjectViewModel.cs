b4792c06bb39c909f01de7021fcf463016cc8b02
jeffmorice <jmorice.anim@gmail.com>
2019-02-06 19:50:08 -0600

    Added logic to construct select lists for media SubTypes. Child SubTypes appear below Parents, like: Video > TV > Episode.

--- BacklogManager/ViewModels/AddMediaObjectViewModel.cs
+++ BacklogManager/ViewModels/AddMediaObjectViewModel.cs
@@ -24,22 +24,64 @@ namespace BacklogManager.ViewModels
         public List<SelectListItem> SubTypes { get; set; }
 
         public AddMediaObjectViewModel() { }
-
+        
         public AddMediaObjectViewModel(IEnumerable<SubType> subTypes)
         {
+            //this list is the accumulator
             SubTypes = new List<SelectListItem>();
 
-            //ToDo: change this code to create a more organized dropdown list
-            foreach (SubType subType in subTypes)
+            //Make a copy of the IEnumerable as a list
+            List<SubType> subTypesList = new List<SubType>(subTypes);
+
+            //Make a copy of the complete list and use it like a de-accumulator to remove already appended IDs.
+            List<SubType> remainingSubTypes = new List<SubType>(subTypes);
+
+            //for each item in list of remaining subtypes
+            //constructs select list with 3 level hierarchy: Parent > Child > GrandChild
+            while (remainingSubTypes.Count > 0)
             {
+                SubType subType = remainingSubTypes[0];
+
+                //add them to the accumulator
                 SubTypes.Add(new SelectListItem
                 {
                     Value = subType.ID.ToString(),
                     Text = subType.Name
                 });
+
+                //remove from the de-accumulator
+                remainingSubTypes.Remove(subType);
+
+                foreach (SubType potentialChild in subTypesList)
+                {
+                    if (potentialChild.ParentID == subType.ID)
+                    {
+                        //When found:
+                        //1) append subtypes to the accumulator list with some annotation like "--" to place them beneath each other.
+                        SubTypes.Add(new SelectListItem
+                        {
+                            Value = potentialChild.ID.ToString(),
+                            Text = "--" + potentialChild.Name
+                        });
+                        //2) remove subtype from remaining list
+                        remainingSubTypes.Remove(potentialChild);
+
+                        foreach (SubType potentialGrandChild in subTypesList)
+                        {
+                            if (potentialGrandChild.ParentID == potentialChild.ID)
+                            {
+                                SubTypes.Add(new SelectListItem
+                                {
+                                    Value = potentialGrandChild.ID.ToString(),
+                                    Text = "----" + potentialGrandChild.Name
+                                });
+                                
+                                remainingSubTypes.Remove(potentialGrandChild);
+                            }
+                        }
+                    }
+                }
             }
         }
-
-        
     }
 }
