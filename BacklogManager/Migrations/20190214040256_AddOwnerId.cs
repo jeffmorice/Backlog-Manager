cac800bddfc93879893505729667d30daa34d3fe
jeffmorice <jmorice.anim@gmail.com>
2019-02-20 22:31:54 -0600

    Added OwnerId property to MediaObject model and updated database.

---
+++ BacklogManager/Migrations/20190214040256_AddOwnerId.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BacklogManager.Migrations
+{
+    public partial class AddOwnerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "MediaObjects",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "MediaObjects");
+        }
+    }
+}
