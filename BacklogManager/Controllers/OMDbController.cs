10e0d08ad818bbdc8c1222b624e2c80bdd7e8928
jeffmorice <jmorice.anim@gmail.com>
2019-02-27 22:11:42 -0600

    Created OMDbController for retrieving IMDB data. Created OMDbTitle class for storing relevant JSON results. Created temporary OMDb Index view for displaying request results.

---
+++ BacklogManager/Controllers/OMDbController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BacklogManager.Common;
+using BacklogManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BacklogManager.Controllers
+{   
+    public class OMDbController : Controller
+    {
+        //Class containing API key
+        OMDbClient omdbClient = new OMDbClient();
+        //Hosted web API REST Service base url
+        string baseUrl = "https://www.omdbapi.com/";
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            //use try, except here to catch network errors and user input errors
+            List<OMDbTitle> omdbTitles = new List<OMDbTitle>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(baseUrl);
+
+                client.DefaultRequestHeaders.Clear();
+
+                //Define request data format (default for omdb is JSON, may be extraneous)
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource
+                //I predict I will need to tweak how the request string is constructed.
+                HttpResponseMessage response = await client.GetAsync(baseUrl + "?apikey=" + omdbClient.ApiKey + "&s=Blade%20Runner");
+
+                //Checking if the response, sent using HttpClient, is successful or not
+                if (response.IsSuccessStatusCode)
+                {
+                    omdbTitles = JsonToListOmdbTitles(response);
+                }
+                //returning OMDbTitle list to view
+                return View(omdbTitles);
+                //return View("Index", responseString);
+            }
+        }
+
+        private List<OMDbTitle> JsonToListOmdbTitles(HttpResponseMessage response)
+        {
+            List<OMDbTitle> omdbTitles = new List<OMDbTitle>();
+
+            //Storing the response details received from web api
+            string responseString = response.Content.ReadAsStringAsync().Result;
+
+            //if the string begins with '{"Search":', extract just the search results
+            if (responseString.Substring(0,10) == "{\"Search\":")
+                {
+                string[] searchArray = responseString.Split(new char[] { '[', ']' });
+
+                responseString = searchArray[1];
+                }
+            
+            //add brackets to turn the response string into an array
+            responseString = "[" + responseString + "]";
+
+            //Deserializing the response received from web api and storing in the OMDbTitle list
+            omdbTitles = JsonConvert.DeserializeObject<List<OMDbTitle>>(responseString);
+
+            return omdbTitles;
+        }
+    }
+}
---
+++ BacklogManager/Models/OMDbTitle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BacklogManager.Models
+{
+    public class OMDbTitle
+    {
+        //This class is for use in deserializing JSON data from OMDb.
+        //Objects of this class are not meant to be stored in the database.
+        //I imagine this class can be used as a ViewModel and pass relevant details (like IMDB ID) to new MediaObjects (MVP), but later may also be used to display details of a particular title on its individual page.
+
+        //so far, pertinent key values common to all IMDB titles are Title, imdbID, and Type (movie, series, or episode)
+        //these values can be passed to new MediaObjects (probably through the existing ViewModel)
+
+        [Key]
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+    }
+}
