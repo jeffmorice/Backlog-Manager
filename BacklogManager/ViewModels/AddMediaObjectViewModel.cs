4374f08a15c4ac6addb59d71460e6c5c6c41f234
jeffmorice <jmorice.anim@gmail.com>
2019-03-04 10:39:10 -0600

    Modified AddMediaObjectViewModel. Added descriptive instructions for interest field. Set 10 as the default value for interest, instead of the default 0.

--- BacklogManager/ViewModels/AddMediaObjectViewModel.cs
+++ BacklogManager/ViewModels/AddMediaObjectViewModel.cs
@@ -24,7 +24,8 @@ namespace BacklogManager.ViewModels
         public string RecommendSource { get; set; }
         public string ExternalId { get; set; }
         [Range(1, 10)]
-        public int Interest { get; set; }
+        [Display(Name = "Rate your interest from 1 to 10 (from 'If I ever get around to it' to 'I literally can't wait').")]
+        public int Interest { get; set; } = 10;    //10 is the default value, here and for the creation of media objects
 
         //ToDo: fix bug where select list is destroyed after invalid submission
         public List<SelectListItem> SubTypes { get; set; }
