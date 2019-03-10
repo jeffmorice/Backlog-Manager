490edddf9d60ad2843c8624be8a8fd30053b3924
jeffmorice <jmorice.anim@gmail.com>
2019-03-10 01:33:56 -0600

    Implemented Image display in Search page.
    Modified OMDbTitle and to accept a link to an image. Modified Search view to display poster images alongside results.

--- BacklogManager/Models/OMDbTitle.cs
+++ BacklogManager/Models/OMDbTitle.cs
@@ -20,5 +20,6 @@ namespace BacklogManager.Models
         public string Title { get; set; }
         public string Type { get; set; }
         public string Year { get; set; }
+        public string Poster { get; set; }
     }
 }
